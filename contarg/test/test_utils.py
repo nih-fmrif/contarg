ffdf7598f34cc32b87dec40a7b12d7c3ac15b640
Dylan Nielson <adenosine@gmail.com>
2023-03-20 09:23:02 -0400

    add bids paths and relpath tools

--- contarg/test/test_utils.py
+++ contarg/test/test_utils.py
@@ -3,7 +3,14 @@ from pathlib import Path
 import nilearn as nl
 from nilearn import image, masking
 import numpy as np
-from contarg.utils import cluster, build_bidsname, parse_bidsname, update_bidspath, get_rel_path, make_rel_symlink
+from contarg.utils import (
+    cluster,
+    build_bidsname,
+    parse_bidsname,
+    update_bidspath,
+    get_rel_path,
+    make_rel_symlink,
+)
 import pytest
 import tempfile
 import shutil
@@ -39,89 +46,110 @@ def test_cluster():
 
 
 def test_parse_and_build_bidsname():
-    test_name = 'sub-02_ses-1_task-rest_run-01_echo-2_acq-1.2.3.4_bad-wrong_break_me_space-T1w_desc-preproc_bold.nii.gz'
+    test_name = "sub-02_ses-1_task-rest_run-01_echo-2_acq-1.2.3.4_bad-wrong_break_me_space-T1w_desc-preproc_bold.nii.gz"
     with pytest.raises(ValueError):
         build_bidsname(parse_bidsname(test_name))
-    test_name = 'func/sub-02_ses-1_task-rest_run-01_echo-2_acq-1.2.3.4_space-T1w_desc-preproc_bold.nii.gz'
-    expected_result = 'func/sub-02_ses-1_task-rest_acq-1.2.3.4_run-01_echo-2_space-T1w_desc-preproc_bold.nii.gz'
+    test_name = "func/sub-02_ses-1_task-rest_run-01_echo-2_acq-1.2.3.4_space-T1w_desc-preproc_bold.nii.gz"
+    expected_result = "func/sub-02_ses-1_task-rest_acq-1.2.3.4_run-01_echo-2_space-T1w_desc-preproc_bold.nii.gz"
     assert build_bidsname(parse_bidsname(test_name)) == expected_result
-    test_name = 'foo/func/sub-02_ses-1_task-rest_acq-1.2.3.4_run-01_echo-2_space-T1w_desc-preproc_bold.nii.gz'
-    assert build_bidsname(parse_bidsname(test_name)) == 'func/sub-02_ses-1_task-rest_acq-1.2.3.4_run-01_echo-2_space-T1w_desc-preproc_bold.nii.gz'
+    test_name = "foo/func/sub-02_ses-1_task-rest_acq-1.2.3.4_run-01_echo-2_space-T1w_desc-preproc_bold.nii.gz"
+    assert (
+        build_bidsname(parse_bidsname(test_name))
+        == "func/sub-02_ses-1_task-rest_acq-1.2.3.4_run-01_echo-2_space-T1w_desc-preproc_bold.nii.gz"
+    )
 
 
 def test_update_bidspath():
     # Test 1: Update sub and ses, exclude ses
-    orig_path = 'sub-01/ses-01/anat/sub-01_ses-01_T1w.nii.gz'
-    new_bids_root = '/data/bids'
-    updates = {'sub': '02', 'ses': '02'}
-    exclude = ['ses']
-    order = ['type', 'sub', 'acq', 'task', 'run', 'modality', 'suffix', 'extension']
-    expected_result = '/data/bids/sub-02/anat/sub-02_T1w.nii.gz'
+    orig_path = "sub-01/ses-01/anat/sub-01_ses-01_T1w.nii.gz"
+    new_bids_root = "/data/bids"
+    updates = {"sub": "02", "ses": "02"}
+    exclude = ["ses"]
+    order = ["type", "sub", "acq", "task", "run", "modality", "suffix", "extension"]
+    expected_result = "/data/bids/sub-02/anat/sub-02_T1w.nii.gz"
 
-    result = update_bidspath(orig_path, new_bids_root, updates, exists=False, exclude=exclude, order=order)
+    result = update_bidspath(
+        orig_path, new_bids_root, updates, exists=False, exclude=exclude, order=order
+    )
 
     assert result.as_posix() == expected_result
 
     # Test 2: Update sub and ses, include ses
-    orig_path = 'sub-01/ses-01/anat/sub-01_ses-01_T1w.nii.gz'
-    new_bids_root = '/data/bids'
-    updates = {'sub': '02', 'ses': '02'}
+    orig_path = "sub-01/ses-01/anat/sub-01_ses-01_T1w.nii.gz"
+    new_bids_root = "/data/bids"
+    updates = {"sub": "02", "ses": "02"}
     exclude = []
-    expected_result = '/data/bids/sub-02/ses-02/anat/sub-02_ses-02_T1w.nii.gz'
+    expected_result = "/data/bids/sub-02/ses-02/anat/sub-02_ses-02_T1w.nii.gz"
 
-    result = update_bidspath(orig_path, new_bids_root, updates, exists=False, exclude=exclude)
+    result = update_bidspath(
+        orig_path, new_bids_root, updates, exists=False, exclude=exclude
+    )
 
     assert result.as_posix() == expected_result
 
     # Test 3: Update sub, ses and acq, include ses and acq
-    orig_path = 'sub-01/ses-01/anat/acq-mprage/sub-01_ses-01_acq-mprage_T1w.nii.gz'
-    new_bids_root = '/data/bids'
-    updates = {'sub': '02', 'ses': '02', 'acq': 'mag', 'type': 'func', 'suffix': 'bold', 'task':'rest'}
+    orig_path = "sub-01/ses-01/anat/acq-mprage/sub-01_ses-01_acq-mprage_T1w.nii.gz"
+    new_bids_root = "/data/bids"
+    updates = {
+        "sub": "02",
+        "ses": "02",
+        "acq": "mag",
+        "type": "func",
+        "suffix": "bold",
+        "task": "rest",
+    }
     exclude = []
-    expected_result = '/data/bids/sub-02/ses-02/func/sub-02_ses-02_task-rest_acq-mag_bold.nii.gz'
+    expected_result = (
+        "/data/bids/sub-02/ses-02/func/sub-02_ses-02_task-rest_acq-mag_bold.nii.gz"
+    )
 
-    result = update_bidspath(orig_path, new_bids_root, updates, exists=False, exclude=exclude)
+    result = update_bidspath(
+        orig_path, new_bids_root, updates, exists=False, exclude=exclude
+    )
 
     assert result.as_posix() == expected_result
 
     # Test 4: Update sub and ses, with `exists=True`, raises FileNotFoundError if the new path doesn't exist
-    orig_path = 'sub-01/ses-01/anat/sub-01_ses-01_T1w.nii.gz'
-    new_bids_root = '/data/bids'
-    updates = {'sub': '02', 'ses': '02'}
+    orig_path = "sub-01/ses-01/anat/sub-01_ses-01_T1w.nii.gz"
+    new_bids_root = "/data/bids"
+    updates = {"sub": "02", "ses": "02"}
     exclude = []
-    expected_result = '/data/bids/sub-02/ses-01/anat/sub-02_ses-01_T1w.nii.gz'
+    expected_result = "/data/bids/sub-02/ses-01/anat/sub-02_ses-01_T1w.nii.gz"
 
     with pytest.raises(FileNotFoundError):
-        result = update_bidspath(orig_path, new_bids_root, updates, exists=True, exclude=exclude)
+        result = update_bidspath(
+            orig_path, new_bids_root, updates, exists=True, exclude=exclude
+        )
 
 
 def test_get_rel_path():
     # test if the function returns the correct relative path
-    assert get_rel_path('a/b/c/d', 'a/e/f') == Path('../../e/f')
-    assert get_rel_path('a/b/c/d', 'a/b/c/f') == Path('f')
-    assert get_rel_path('a/b/c/d', 'a/b/c/d/e/f') == Path('e/f')
-    assert get_rel_path('a/b/c/d', 'a/b/c/d') == Path('.')
-    assert get_rel_path('/a/b/c/d', '/e/f/g/h') == Path('../../../e/f/g/h')
-
-if __name__ == "__main__":
-    test_cluster()
+    assert get_rel_path("a/b/c/d", "a/e/f") == Path("../../e/f")
+    assert get_rel_path("a/b/c/d", "a/b/c/f") == Path("f")
+    assert get_rel_path("a/b/c/d", "a/b/c/d/e/f") == Path("e/f")
+    assert get_rel_path("a/b/c/d", "a/b/c/d") == Path(".")
+    assert get_rel_path("/a/b/c/d", "/e/f/g/h") == Path("../../../e/f/g/h")
 
 
 def test_make_rel_symlink():
     # create temporary directories and files for testing
     with tempfile.TemporaryDirectory() as tempdir:
-        source_dir = Path(tempdir) / 'source'
-        target_dir = Path(tempdir) / 'target'
+        source_dir = Path(tempdir) / "source"
+        target_dir = Path(tempdir) / "target"
         source_dir.mkdir()
         target_dir.mkdir()
-        (target_dir / 'file.txt').touch()
+        (target_dir / "file.txt").touch()
 
         # test if the function creates a relative symlink
-        symlink_path = source_dir / 'symlink'
-        make_rel_symlink(symlink_path, target_dir / 'file.txt')
+        symlink_path = source_dir / "symlink"
+        make_rel_symlink(symlink_path, target_dir / "file.txt")
         assert symlink_path.is_symlink()
-        assert symlink_path.resolve() == (target_dir / 'file.txt').resolve()
+        assert symlink_path.resolve() == (target_dir / "file.txt").resolve()
 
         # test if the function raises an error for an existing symlink that doesn't point to the target
         with pytest.raises(ValueError):
             make_rel_symlink(symlink_path, target_dir)
+
+
+if __name__ == "__main__":
+    test_cluster()
