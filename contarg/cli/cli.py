09994709deaa3fcd5fdf7a1df0b75a33f52493b5
Dylan Nielson <adenosine@gmail.com>
2023-03-27 10:52:39 -0400

    Add tans and provisional code for pfm

--- contarg/cli/cli.py
+++ contarg/cli/cli.py
@@ -1,8 +1,11 @@
 import click
 from .run_hierarchical import contarg
 from .run_seedmap import contarg as contargsm
+from .run_tans import contarg as contargtans
+from .run_pfm import contarg as contargpfm
 
-contarg = click.CommandCollection(sources=[contarg, contargsm])
+
+contarg = click.CommandCollection(sources=[contarg, contargsm, contargtans, contargpfm])
 
 if __name__ == "__main__":
     contarg()
