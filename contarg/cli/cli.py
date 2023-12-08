8467dac6c9512057105682d6764739bfca5f3ae9
Dylan Nielson <adenosine@gmail.com>
2023-12-08 11:04:53 -0500

    add normgrid cli

--- contarg/cli/cli.py
+++ contarg/cli/cli.py
@@ -4,9 +4,11 @@ from .run_seedmap import contarg as contargsm
 from .run_tans import contarg as contargtans
 from .run_pfm import contarg as contargpfm
 from .run_stimgrid import contarg as contargsg
+from .run_normgrid import contarg as contargng
 
 
-contarg = click.CommandCollection(sources=[contarg, contargsm, contargtans, contargpfm, contargsg])
+
+contarg = click.CommandCollection(sources=[contarg, contargsm, contargtans, contargpfm, contargsg, contargng])
 
 if __name__ == "__main__":
     contarg()
