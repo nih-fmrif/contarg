2e6becbe28d2aa0e71524b37a6e4f55e84db3b23
Dylan Nielson <adenosine@gmail.com>
2023-12-20 10:50:58 -0500

    drop run_pfm from cli

--- contarg/cli/cli.py
+++ contarg/cli/cli.py
@@ -2,13 +2,13 @@ import click
 from .run_hierarchical import contarg
 from .run_seedmap import contarg as contargsm
 from .run_tans import contarg as contargtans
-from .run_pfm import contarg as contargpfm
+#from .run_pfm import contarg as contargpfm
 from .run_stimgrid import contarg as contargsg
 from .run_normgrid import contarg as contargng
 
 
 
-contarg = click.CommandCollection(sources=[contarg, contargsm, contargtans, contargpfm, contargsg, contargng])
+contarg = click.CommandCollection(sources=[contarg, contargsm, contargtans, contargsg, contargng])
 
 if __name__ == "__main__":
     contarg()
